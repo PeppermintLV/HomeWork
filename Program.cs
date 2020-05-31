66649eeb1283a130a72c733e36477ee0271469b0
Liene Dārta Lukstiņa <lienedarta@gmail.com>
2020-05-31 23:03:27 +0300

    Majas darbu glabatuve

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HM1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create console application which prints out your name and surname.
+            Console.WriteLine("My name is Darta Liene Lukstina");
+            Console.ReadKey();            
+        }
+    }
+}
